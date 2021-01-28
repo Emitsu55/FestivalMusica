24b504c1a642d3e0f04c553ea1802368bb4227e8
Emitsu <emitsu.studios@gmail.com>
2021-01-28 00:10:42 -0300

    Nuevas dependencias

--- gulpfile.js
+++ gulpfile.js
@@ -27,32 +27,60 @@ exports.tareas = series(css, javascript); //Ejecuta en serie las funciones pasad
 //dest elige la ubicacion donde guardar el archivo compilado
 //Watch vigila un archivo en busca de cambios
  const sass = require('gulp-sass');
+ const imagemin = require('gulp-imagemin');
+ const notify = require('gulp-notify');
+ const webpimg = require('gulp-webp');
+
+//  Paths
+const paths = {
+    imagenes : 'src/img/**/+',
+    scss : 'src/scss/**/*.scss'
+}
 
 
 //funciones
 
 function watchArchivos() {
-    watch('src/scss/**/*.scss', css); // '**' refiere a todas las carpetas  
+    watch(paths.scss); // '**' refiere a todas las carpetas  
 }
 
 function css(done) {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
     .pipe( sass())
     .pipe( sass({
         outputStyle: 'expanded'
     }))
     .pipe( dest('./build/css'))
+    .pipe(notify({ message: 'Codigo SASS transformado a CSS'}));
 }
 
 function minificarCss() {
-    return src ('src/scss/app.scss')
+    return src (paths.scss)
     .pipe( sass({
         outputStyle: 'compressed'
     }))
     .pipe( dest('./build/css'))
+    .pipe(notify({ message: 'Codigo CSS Minificado'}));
+}
+
+function imagenes() {
+    return src(paths.imagenes)
+    .pipe( imagemin())
+    .pipe(dest('./build/img'))
+    .pipe(notify({ message: 'Imagen minificadas'}));
+}
+
+function versionWebp(){
+    return src(paths.imagenes)
+    .pipe(webpimg())
+    .pipe(dest('./build/img'))
+    .pipe(notify({ message: 'version webp lista'}));
 }
 
 //Tareas
 exports.css = css;
 exports.minificarCss = minificarCss;
 exports.watchArchivos = watchArchivos;
+exports.imagenes = imagenes;
+exports.versionWebp =versionWebp;
+// exports.default = series (css, imagenes, watchArchivos); ejecuta en serie todas esas funciones se manda a llamar con 'gulp'

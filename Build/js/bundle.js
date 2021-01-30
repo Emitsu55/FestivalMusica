6582eb636225dbf23ff9a5f6c055dadbb7db6a88
Emitsu <emitsu.studios@gmail.com>
2021-01-30 20:46:50 -0300

    Agregado footer y scroll view

--- Build/js/bundle.js
+++ Build/js/bundle.js
@@ -1,4 +1,23 @@
-console.log('Listo');
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+})
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion_principal a');
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({
+                behavior: 'smooth',
+
+            })
+
+        })
+    })
+}
 document.addEventListener('DOMContentLoaded', function() {
     crearGaleria();
 })   
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('Listo');
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+})
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion_principal a');
+    
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+
+            const seccion = document.querySelector(e.target.attributes.href.value);
+            seccion.scrollIntoView({
+                behavior: 'smooth',
+
+            })
+
+        })
+    })
+}
